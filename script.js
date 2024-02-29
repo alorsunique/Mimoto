aaa87918703f3a91df755f865c301ced71bad15e
alorsunique <sounique7800@gmail.com>
2024-02-29 15:38:05 +0800

    2024 February 29

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const slides = document.querySelectorAll(".slide");
     let currentIndex = 0;
 
